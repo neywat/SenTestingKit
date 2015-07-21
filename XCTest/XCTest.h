e9764d8f4b42a7ef4b2954842006ab45566107fa
Damien Brossard <brossard.damien@gmail.com>
2015-07-21 23:00:52 +0200

    cf. https://github.com/letatas/yafacwes/issues/14 first bits of compatibility layer from SenTestingKit to XCTest

---
+++ XCTest/XCTest.h
@@ -0,0 +1,1 @@
+../XCTestCompat.h
---
+++ XCTestCompat.h
@@ -0,0 +1,18 @@
+//
+//  XCTestCompat.h
+//  XCTest compatibility layer for GNUstep
+//
+//  Created by Damien Brossard on 18/07/2015.
+//
+
+#import <SenTestingKit.h>
+
+// classes
+#define XCTestCase SenTestCase
+
+// macros
+#define XCTAssertEqual(e1,e2) STAssertEquals(e1,e2,@#e1 " - " #e2)
+#define XCTAssertEqualObjects(o1,o2) STAssertEqualObjects(o1,o2,@#o1 " - " #o2)
+#define XCTAssertFalse(e) STAssertFalse(e,@#e)
+#define XCTAssert(e) STAssertTrue(e,@#e)
+#define XCTAssertThrows(e) STAssertThrows(e,@#e)
